0d822762def92269b5f3a334c04f3bda9fb31a49
alxdrian <alex_97piscis@outlook.com>
2021-11-25 05:01:18 -0500

    add protected route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
 import OrderProvider from "./contexts/OrderContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 const cssGlobal = css`
   @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');
@@ -30,7 +31,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<PrivateRoute><Cart /></PrivateRoute>} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,5 @@
+import { Navigate } from "react-router"
+
+export default function PrivateRoute ({children}) {
+  return sessionStorage.getItem('token') ? {children} : <Navigate to="/" />
+}
